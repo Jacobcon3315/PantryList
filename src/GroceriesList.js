2d778276e0643160b28d9a2763d4917c42b18e77
Jacobcon3315 <jake.conacher@gmail.com>
2023-06-27 21:02:44 -0500

    Fixed containers for the list. Moved back to flatlist

--- src/GroceriesList.js
+++ src/GroceriesList.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Text, View, StyleSheet, StatusBar, Dimensions } from "react-native";
-import { FlashList } from "@shopify/flash-list";
+import { Text, View, SafeAreaView, StyleSheet, StatusBar, FlatList } from "react-native";
 
 const DATA = [
   {
@@ -8,19 +7,42 @@ const DATA = [
   },
   {
     title: "Bacon",
-  },
+  }
 ];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 function GroceriesListPage() {
     return (
-      <View style={{ height: 200, width: Dimensions.get("screen").width, flex: 1, justifyContent: "center", alignItems: "center"}}>
-        <FlashList
+      <SafeAreaView style={styles.container}>
+      <FlatList
         data={DATA}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
-        estimatedItemSize={50}
-        />
-      </View>
+        renderItem={({item}) => <Item title={item.title} />}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
     );
   }
 
-  export default GroceriesListPage;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: "#f6b26b",
+    borderRadius: 10,
+    padding: 20,
+    marginVertical: 5,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+export default GroceriesListPage;
