bed3cc98fbfb6415d6e25727cf0452b914ea3a5c
Jacobcon3315 <jake.conacher@gmail.com>
2023-06-27 21:32:08 -0500

    Moved shoppingitems to different file

--- src/GroceriesList.js
+++ src/GroceriesList.js
@@ -1,48 +1,24 @@
 import React from "react";
-import { Text, View, SafeAreaView, StyleSheet, StatusBar, FlatList } from "react-native";
-
-const DATA = [
-  {
-    title: "Eggs",
-  },
-  {
-    title: "Bacon",
-  }
-];
-
-const Item = ({title}) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
+import { SafeAreaView, StyleSheet, StatusBar, FlatList } from "react-native";
+import { Item, DATA } from "./components/ShoppingItem";
 
 function GroceriesListPage() {
-    return (
-      <SafeAreaView style={styles.container}>
+  return (
+    <SafeAreaView style={styles.container}>
       <FlatList
         data={DATA}
         renderItem={({item}) => <Item title={item.title} />}
         keyExtractor={item => item.id}
       />
     </SafeAreaView>
-    );
-  }
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: "#f6b26b",
-    borderRadius: 10,
-    padding: 20,
-    marginVertical: 5,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
+  }
 });
 
 export default GroceriesListPage;
